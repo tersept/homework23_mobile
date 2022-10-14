94d47c3a1ef0ea4618aecd6a268eefe8ecae5eee
svasenkov <aanher@gmail.com>
2022-10-14 21:19:33 +0300

    moved to testbase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,28 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import drivers.BrowserstackMobileDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
 public class TestBase {
+    @BeforeAll
+    public static void setup() {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        closeWebDriver();
+    }
 }
