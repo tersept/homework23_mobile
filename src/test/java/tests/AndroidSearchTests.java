8e63f405e4ae0613bdf5743d77be83a55d189b02
svasenkov <aanher@gmail.com>
2022-10-14 21:47:05 +0300

    added allure

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -6,15 +6,20 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
 
 
-public class AndroidSearchTests {
+public class AndroidSearchTests extends TestBase {
 
     @Test
     void searchTest() {
-        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
-        $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
+        step("Verify content found", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .shouldHave(sizeGreaterThan(0)));
     }
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,12 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import drivers.BrowserstackMobileDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class TestBase {
     @BeforeAll
@@ -18,11 +20,20 @@ public class TestBase {
 
     @BeforeEach
     public void startDriver() {
+        addListener("AllureSelenide", new AllureSelenide());
+
         open();
     }
 
     @AfterEach
     public void afterEach() {
+        String sessionId = sessionId().toString();
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+
         closeWebDriver();
+
+        Attach.video(sessionId);
     }
 }
