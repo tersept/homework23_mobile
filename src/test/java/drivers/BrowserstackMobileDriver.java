7e912937b765863b0139ff31bbf70e94d0026b43
Irina <pashaginy@rambler.ru>
2022-10-20 16:20:29 +0300

    edited tests

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -1,5 +1,6 @@
 package drivers;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverProvider;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
@@ -12,12 +13,18 @@ import java.net.URL;
 
 public class BrowserstackMobileDriver implements WebDriverProvider {
     Config config = ConfigFactory.create(Config.class, System.getProperties());
-
+    public URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @Override
     public WebDriver createDriver(Capabilities capabilities) {
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
         mutableCapabilities.merge(capabilities);
-
+        Configuration.remote = config.getRemoteUrl();
         // Set your access credentials
         mutableCapabilities.setCapability("browserstack.user", config.getBrowserstackUser());
         mutableCapabilities.setCapability("browserstack.key", config.getBrowserstackKey());
@@ -37,13 +44,5 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
         // Initialise the remote Webdriver using BrowserStack remote URL
         // and desired capabilities defined above
         return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
-    }
-
-    public URL getBrowserstackUrl() {
-        try {
-            return new URL("http://hub.browserstack.com/wd/hub");
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
